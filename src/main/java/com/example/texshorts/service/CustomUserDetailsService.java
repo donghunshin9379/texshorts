61cc5137f3e25669780ea9366ec2eea9add24266
hun9379 <powershin97@icloud.com>
2025-07-10 19:55:48 +0900

    25.07.10 (CustomUserDetails, PostReaction)

--- src/main/java/com/example/texshorts/service/CustomUserDetailsService.java
+++ src/main/java/com/example/texshorts/service/CustomUserDetailsService.java
@@ -3,17 +3,13 @@ package com.example.texshorts.service;
 import com.example.texshorts.custom.CustomUserDetails;
 import com.example.texshorts.entity.User;
 import com.example.texshorts.repository.UserRepository;
-import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-
-import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -28,7 +24,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("해당 사용자가 존재하지 않습니다"));
+                .orElseThrow(() -> new UsernameNotFoundException("사용자가 존재하지 않습니다"));
         logger.info("로그인 :{}", user.getUsername());
         logger.info("계정권한 :{}", user.getRoles());
 
